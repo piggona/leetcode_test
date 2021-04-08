96f3e332bdc056ddfa7a525d7e0271455b6185a7
yuanhaojin <yuanhaojin@tencent.com>
2021-04-08 09:24:29 +0800

    find minimum in rotated sorted array

---
+++ find_minimum_in_rotated_sorted_array/min.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func findMin(nums []int) int {
+	left := 0
+	right := len(nums) - 1
+	for left < right {
+		mid := left + (right-left)/2
+		if nums[mid] < nums[right] {
+			right = mid
+		} else {
+			left = mid + 1
+		}
+	}
+	return nums[left]
+}
+
+func main() {
+	fmt.Println(findMin([]int{0, 1, 2}))
+}
